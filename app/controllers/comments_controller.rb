e9af9521c8d2987d1c5ea4dbdd5d16041756034b
Julianne Johnson <tweetypie417@yahoo.com>
2018-10-18 22:43:35 -0500

    comment edit and delete functionality incomplete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,34 @@
 class CommentsController < ApplicationController
+  def edit
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comments.find(params[:id])
+  end
+
 	def create
+		# Add a new comment to the post
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
+  end
+
+  def update
+  	# edit comment
+  	@comment = Comment.find(params[:id])
+
+  	if @comment.update(comment_params)
+	    redirect_to @comment
+	  else
+	    render 'edit'
+	  end
+  end
+
+  def destroy
+  	# Remove a comment from the database
+  	@post = Post.find(params[:post_id])
+  	@comment = Comment.find(params[:id])
+  	@comment.destroy
+
+  	redirect_to post_path(@post)
   end
  
   private
