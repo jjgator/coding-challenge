48da9b23685aa0677b9ab3bc4c70e019f8a94a75
Julianne Johnson <tweetypie417@yahoo.com>
2018-10-18 20:09:00 -0500

    post actions index, show, new create defined and functioning, error handling added for create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,35 @@
 class PostsController < ApplicationController
   def index
-    # Return all `Post`
+    # Return all `Posts`
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
     # Return view to create a new Post
+    @post = Post.new
   end
 
   def create
     # Add a new `Post` to the database
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def destroy
     # Remove a `Post` from the database
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
